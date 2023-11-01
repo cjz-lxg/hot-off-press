04b318d638e920327e39c30b8bc2a0b84e8fb925
Russell <18960850393@163.com>
2023-11-01 21:48:34 +0800

    22:基于es与kafka完成了在微服务端发送文章时自动为文章添加索引

--- hot-off-press-service/hot-off-press-article/src/main/java/com/russel/article/service/impl/ArticleFreemarkerServiceImpl.java
+++ hot-off-press-service/hot-off-press-article/src/main/java/com/russel/article/service/impl/ArticleFreemarkerServiceImpl.java
@@ -1,17 +1,23 @@
 package com.russel.article.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.russel.article.mapper.ApArticleContentMapper;
 import com.russel.article.service.ApArticleService;
 import com.russel.article.service.ArticleFreemarkerService;
+import com.russel.common.constants.ArticleConstants;
 import com.russel.file.service.FileStorageService;
 import com.russel.model.article.pojos.ApArticle;
+import com.russel.model.search.vos.SearchArticleVo;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,6 +49,7 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
     private ApArticleService apArticleService;
 
     @Override
+    @Async
     public void buildArticleToMinIO(ApArticle apArticle, String content) {
         //已知文章的id
         //4.1 获取文章内容
@@ -69,6 +76,26 @@ public class ArticleFreemarkerServiceImpl implements ArticleFreemarkerService {
             //4.4 修改ap_article表，保存static_url字段
             apArticleService.update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId,apArticle.getId())
                     .set(ApArticle::getStaticUrl,path));
+
+            createArticleESIndex(apArticle,content,path);
         }
+    }
+
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    /**
+     * 送消息，创建索引
+     * @param apArticle
+     * @param content
+     * @param path
+     */
+    private void createArticleESIndex(ApArticle apArticle, String content, String path) {
+        SearchArticleVo vo = new SearchArticleVo();
+        BeanUtils.copyProperties(apArticle,vo);
+        vo.setContent(content);
+        vo.setStaticUrl(path);
+
+        kafkaTemplate.send(ArticleConstants.ARTICLE_ES_SYNC_TOPIC, JSON.toJSONString(vo));
     }
 }
