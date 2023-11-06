dab448a9bce6b82bf50c33842637227dc650dcab
Russell <18960850393@163.com>
2023-11-06 09:02:05 +0800

    26:将基于mongoDB完成删除搜索记录

---
+++ hot-off-press-service/hot-off-press-search/src/main/java/com/russel/search/dtos/HistorySearchDto.java
@@ -0,0 +1,15 @@
+package com.russel.search.dtos;
+
+import lombok.Data;
+
+/**
+ * @author Russel
+ * @DATE 2023/11/6.
+ */
+@Data
+public class HistorySearchDto {
+    /**
+     * 接收搜索历史记录id
+     */
+    String id;
+}
